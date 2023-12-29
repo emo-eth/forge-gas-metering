26831616c5c1b5b53cea4c8064d00d1a562a5ae7
James Wenzel <wenzel.james.r@gmail.com>
2023-12-29 08:10:02 -0800

    use constants

--- src/OpStackMetering.sol
+++ src/OpStackMetering.sol
@@ -14,7 +14,9 @@ import {LibString} from "solady/utils/LibString.sol";
 contract OpStackMetering is MainnetMetering {
     // todo: read these values from op-mainnet contract
     uint256 constant L1_FEE_SCALAR_BPS = 6840;
+    uint256 constant BPS_DENOM = 10_000;
     uint256 constant L1_OVERHEAD = 188;
+    uint256 constant L1_RLP_OVERHEAD = 112 * 16; // assume ~112 bytes; 16 gas per byte
 
     function getCallOverhead(TransactionData memory data)
         internal
@@ -24,23 +26,28 @@ contract OpStackMetering is MainnetMetering {
         returns (uint256)
     {
         uint256 callDataCosts = callDataCost(data.callData);
-        uint256 l1Costs = 188 + 1792 + callDataCosts;
+        uint256 l1Costs = L1_OVERHEAD + L1_RLP_OVERHEAD + callDataCosts;
+        console2.log(
+            string.concat(
+                "~L1 Gas breakdown: ",
+                LibString.toString(L1_OVERHEAD),
+                " (constant) + ~",
+                LibString.toString(L1_RLP_OVERHEAD),
+                " (RLP) + ",
+                LibString.toString(callDataCosts),
+                " (calldata)"
+            )
+        );
         console2.log(
             string.concat(
                 "~L1 Gas cost     : ",
                 LibString.toString(l1Costs),
                 " gas (before), ",
-                LibString.toString(l1Costs * L1_FEE_SCALAR_BPS / 10_000),
+                LibString.toString(l1Costs * L1_FEE_SCALAR_BPS / BPS_DENOM),
                 " gas (after scalar multiplier)"
             )
         );
-        console2.log(
-            string.concat(
-                "~L1 Gas breakdown: 188 (constant) + ~1792 (RLP) + ",
-                LibString.toString(callDataCosts),
-                " (calldata)"
-            )
-        );
+
         return super.getCallOverhead(data);
     }
 }
